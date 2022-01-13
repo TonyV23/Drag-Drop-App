976ba0d40a5385a258ca0d73f52d30847d8fc800
viper23 <htonnycarlos@gmail.com>
2022-01-13 10:45:56 +0200

    finish js

---
+++ index.js
@@ -0,0 +1,18 @@
+let item ;
+
+document.addEventListener("dragstart", (e) =>{
+   item = e.target; 
+});
+
+document.addEventListener("dragover", (e) =>{
+    e.preventDefault();
+});
+
+document.addEventListener("drop", (e) =>{
+    if (e.target.getAttribute("data-draggable") == "target") {
+        e.preventDefault();
+        e.target.appendChild(item);
+    }
+});
+
+document.addEventListener("dragend", () => (item = null));
